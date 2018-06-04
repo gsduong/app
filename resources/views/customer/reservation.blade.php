b613ff093fe2bfac9addc09a150f68363dd229a8
gsduong <nguyenduong.ict.hust@gmail.com>
2018-06-04 23:47:55 +0700

    Debug

--- resources/views/customer/reservation.blade.php
+++ resources/views/customer/reservation.blade.php
@@ -1,7 +1,7 @@
 @extends('customer.index')
 
 @section('title')
-    New booking order for {{$restaurant->name}}
+    RESERVATION
 @endsection
 
 @section('extra-css')
@@ -18,43 +18,6 @@
             <form method="POST" action="{{-- {{route('reservation.create', ['restaurant_slug' => $restaurant->slug])}} --}}">
                 @csrf
                 <div class="row clearfix">
-                    <div class="col-xs-12">
-                        <b>Date</b>
-                        <div class="input-group" style="margin-bottom: 0;">
-                            <span class="input-group-addon">
-                                <i class="material-icons">date_range</i>
-                            </span>
-                            <div class="form-line">
-                                <input type="date" class="form-control" name="date" value="{{ old('date') }}" required="true" min={{date('Y-m-d')}}>
-                            </div>
-                        </div>
-                        <label id="date-error" class="validation-error-label" for="date"><small>{{ $errors->first('date') }}</small></label>
-                    </div>
-                    <div class="col-xs-12">
-                        <b>Time</b>
-                        <div class="input-group" style="margin-bottom: 0;">
-                            <span class="input-group-addon">
-                                <i class="material-icons">access_time</i>
-                            </span>
-                            <div class="form-line">
-                                <input type="time" value="{{ old('time') }}" class="form-control" name="time" placeholder="Ex: 23:59" required="true">
-                            </div>
-                        </div>
-                        <label id="time-error" class="validation-error-label" for="time"><small>{{ $errors->first('time') }}</small></label>
-                    </div>
-                    @if($restaurant->contacts->count() > 0)
-                        <div class="col-xs-12">
-                            <b>Branch</b>
-                            <div class="input-group" style="margin-bottom: 0;">
-                                <div class="demo-radio-button">
-                                    @foreach($restaurant->contacts as $no => $contact)
-                                    <input name="address_id" type="radio" id="address_{{$no + 1}}" {{$no == 0 ? 'checked' : ''}} value="{{$contact->id}}">
-                                    <label for="address_{{$no + 1}}">{{$contact->address}}</label>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-                    @endif
                     <div class="col-xs-12">
                         <b>Name</b>
                         <div class="input-group" style="margin-bottom: 0;">
@@ -79,25 +42,62 @@
                         </div>
                         <label id="phone-error" class="validation-error-label" for="phone"><small>{{ $errors->first('phone') }}</small></label>
                     </div>
-                    <div class="col-xs-12">
+                    <div class="col-xs-6">
+                        <b>Date</b>
+                        <div class="input-group" style="margin-bottom: 0;">
+                            <span class="input-group-addon">
+                                <i class="material-icons">date_range</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="date" class="form-control" name="date" value="{{ old('date') }}" required="true" min={{date('Y-m-d')}}>
+                            </div>
+                        </div>
+                        <label id="date-error" class="validation-error-label" for="date"><small>{{ $errors->first('date') }}</small></label>
+                    </div>
+                    <div class="col-xs-6">
+                        <b>Time</b>
+                        <div class="input-group" style="margin-bottom: 0;">
+                            <span class="input-group-addon">
+                                <i class="material-icons">access_time</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="time" value="{{ old('time') }}" class="form-control" name="time" placeholder="Ex: 23:59" required="true">
+                            </div>
+                        </div>
+                        <label id="time-error" class="validation-error-label" for="time"><small>{{ $errors->first('time') }}</small></label>
+                    </div>
+                    @if($restaurant->contacts->count() > 0)
+                        <div class="col-xs-12">
+                            <b>Branch</b>
+                            <div class="input-group" style="margin-bottom: 0;">
+                                <div class="demo-radio-button">
+                                    @foreach($restaurant->contacts as $no => $contact)
+                                    <input name="address_id" type="radio" id="address_{{$no + 1}}" {{$no == 0 ? 'checked' : ''}} value="{{$contact->id}}">
+                                    <label for="address_{{$no + 1}}">{{$contact->address}}</label>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    <div class="col-xs-6">
                         <b>Adults</b>
                         <div class="input-group" style="margin-bottom: 0;">
                             <span class="input-group-addon">
                                 <i class="material-icons">people</i>
                             </span>
                             <div class="form-line">
-                                <input type="number" name="adult" class="form-control" placeholder="Number of adults" required="true" min="1" value="{{ old('adult') }}">
+                                <input type="number" name="adult" class="form-control" placeholder="Adults" required="true" min="1" value="{{ old('adult') }}">
                             </div>
                         </div>
                     </div>
-                    <div class="col-xs-12">
+                    <div class="col-xs-6">
                         <b>Children</b>
                         <div class="input-group" style="margin-bottom: 0;">
                             <span class="input-group-addon">
                                 <i class="material-icons">child_care</i>
                             </span>
                             <div class="form-line">
-                                <input type="number" name="children" class="form-control" placeholder="Number of children" value="{{ old('children') }}" min="0">
+                                <input type="number" name="children" class="form-control" placeholder="Children" value="{{ old('children') }}" min="0">
                             </div>
                         </div>
                         <label id="children-error" class="validation-error-label" for="children"><small>{{ $errors->first('children') }}</small></label>
