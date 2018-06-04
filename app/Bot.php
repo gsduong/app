4e097a942eae7c42d11d3e2fb910782caf534b75
gsduong <nguyenduong.ict.hust@gmail.com>
2018-06-04 13:29:45 +0700

    Debug

--- app/Bot.php
+++ app/Bot.php
@@ -25,45 +25,47 @@ class Bot extends Model
 
     public function getActiveFieldsForDefaultResponse() {
         $settings = json_decode($this->settings, true);
+        file_put_contents("php://stderr", $settings);
+        file_put_contents("php://stderr", $this->settings);
         if (!$settings) {
              file_put_contents("php://stderr", 'Bot settings is null!');
             return null;
         }
-        $fields = array();
-        foreach ($settings as $key => $value) {
-            if ($value["active"]) {
-                switch ($key) {
-                    case 'menu':
-                        $fields[$key] = array("payload" => "MENU_PAYLOAD", "name" => "Menu");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    case 'order':
-                        $fields[$key] = array("payload" => null, "name" => "Order đồ ăn");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    case 'booking':
-                        $fields[$key] = array("payload" => "BOOKING_PAYLOAD", "name" => "Đặt bàn");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    case 'chat_with_staff':
-                        $fields[$key] = array("payload" => "STAFF_PAYLOAD", "name" => "Chat với nhân viên");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    case 'address':
-                        $fields[$key] = array("payload" => null, "name" => "Địa chỉ");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    case 'opening_hour':
-                        $fields[$key] = array("payload" => null, "name" => "Giờ mở cửa");
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                    default:
-                        file_put_contents("php://stderr", $fields);
-                        break;
-                }
-            }
-        }
-        return $fields;
+        // $fields = array();
+        // foreach ($settings as $key => $value) {
+        //     if ($value["active"]) {
+        //         switch ($key) {
+        //             case 'menu':
+        //                 $fields[$key] = array("payload" => "MENU_PAYLOAD", "name" => "Menu");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             case 'order':
+        //                 $fields[$key] = array("payload" => null, "name" => "Order đồ ăn");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             case 'booking':
+        //                 $fields[$key] = array("payload" => "BOOKING_PAYLOAD", "name" => "Đặt bàn");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             case 'chat_with_staff':
+        //                 $fields[$key] = array("payload" => "STAFF_PAYLOAD", "name" => "Chat với nhân viên");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             case 'address':
+        //                 $fields[$key] = array("payload" => null, "name" => "Địa chỉ");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             case 'opening_hour':
+        //                 $fields[$key] = array("payload" => null, "name" => "Giờ mở cửa");
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //             default:
+        //                 file_put_contents("php://stderr", $fields);
+        //                 break;
+        //         }
+        //     }
+        // }
+        // return $fields;
     }
 
     // public function getPostbackButtonsForDefaultResponse(){
