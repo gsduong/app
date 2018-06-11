fc68cb5413f246e5f793e9c2c98a8bf89bb6235f
gsduong <nguyenduong.ict.hust@gmail.com>
2018-06-11 10:56:04 +0700

    Fix typo

--- database/migrations/2018_06_04_161737_add_column_to_table.php
+++ database/migrations/2018_06_04_161737_add_column_to_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnToTable extends Migration
+class AddColumnToTableRestaurants extends Migration
 {
     /**
      * Run the migrations.
