3840cdb5cd7780eb52ce63dae150e377c84319b9
gsduong <nguyenduong.ict.hust@gmail.com>
2018-06-11 10:58:10 +0700

    Fix typo

--- database/migrations/2018_06_07_204819_add_column_to_table_orders.php
+++ database/migrations/2018_06_07_204819_add_column_to_table_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddMoneyColumnToTableOrders extends Migration
+class AddColumnToTableOrders extends Migration
 {
     /**
      * Run the migrations.
