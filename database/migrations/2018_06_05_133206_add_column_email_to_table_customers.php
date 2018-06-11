fa653e9dd32c4b962a4b44a4460603eae25692da
gsduong <nguyenduong.ict.hust@gmail.com>
2018-06-11 10:53:29 +0700

    Fix typo

--- database/migrations/2018_06_05_133206_add_column_email_to_table_customers.php
+++ database/migrations/2018_06_05_133206_add_column_email_to_table_customers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnEmailToTable extends Migration
+class AddColumnEmailToTableCustomers extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_06_06_004946_add_column_email_to_table_reservations.php
+++ database/migrations/2018_06_06_004946_add_column_email_to_table_reservations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnEmailToTable extends Migration
+class AddColumnEmailToTableReservations extends Migration
 {
     /**
      * Run the migrations.
